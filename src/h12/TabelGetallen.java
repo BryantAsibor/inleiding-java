b5668ea8cf10f196d5a4c188f7f7c44ed7be3606
BryantAsibor <2136149@talnet.nl>
2020-10-09 03:08:24 +0200

    12.3

---
+++ src/h12/TabelGetallen.java
@@ -0,0 +1,14 @@
+package h12;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Getallen extends Applet {
+
+    TextField[] input = new TextField[5];
+    Button confirm;
+    int[] getal = new int[5];
+
+
+}
