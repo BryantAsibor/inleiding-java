9e24863f8f2731424660668fdb2edd72c19c064f
BryantAsibor <2136149@talnet.nl>
2020-10-09 03:09:39 +0200

    12.3

--- src/h12/TabelGetallen.java
+++ src/h12/TabelGetallen.java
@@ -10,5 +10,18 @@ public class Getallen extends Applet {
     Button confirm;
     int[] getal = new int[5];
 
+    public void init() {
 
+        int teller;
+
+        for (teller = 0; teller < input.length; teller++) {
+
+            input[teller] = new TextField("", 10);
+            input[teller].addActionListener(new Listener());
+            add(input[teller]);
+        }
+        confirm = new Button("ok");
+        confirm.addActionListener(new Listener());
+        add(confirm);
+    }
 }
