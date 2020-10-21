8b216deea1e3e064faf8cdffa3ca95bc4f5faa92
BryantAsibor <2136149@talnet.nl>
2020-10-21 18:23:18 +0200

    12.4 push

--- src/h12/Waarde.java
+++ src/h12/Waarde.java
@@ -18,4 +18,29 @@ public class Waarde extends Applet {
         add(label);
         add(input);
     }
+
+    public void paint(Graphics g){
+        if (gevonden == true) {
+            g.drawString("Gevonden: " + getal, 10 ,40);
+        } else {
+            g.drawString("Niet gevonden", 10, 40);
+        }
+    }
+
+    class Listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String output = input.getText();
+            getal = Integer.parseInt(output);
+            int teller = 0;
+            gevonden = false;
+            while(teller < array.length) {
+                if(array[teller] == getal) {
+                    gevonden = true;
+                }
+                teller ++;
+            }
+            repaint();
+        }
+    }
+
 }
